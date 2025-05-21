d35d816c326d0c6677b8b516ee16f7778e6663e7
tdingsun <tiger.dingsun@gmail.com>
2025-05-21 09:44:55 -0400

    grid styling

--- src/lib/components/GridItem.svelte
+++ src/lib/components/GridItem.svelte
@@ -4,17 +4,28 @@
 
 	let { item } = $props();
 </script>
-<div class="flex flex-col gap-3">
+
+<div class="sm:basis-1/4">
+	<div class="flex flex-col gap-2">
+		{#if item.link}
+			<a href={item.link} target="_blank" class="group sm:basis-1/4 flex flex-col gap-4">
 				{#if item.image}
-						<img src={getThumbnailImg(item.image)} class="max-w-36 self-center" />
-					{/if}
-					{#if item.header}
-						<div class="font-bold leading-snug text-lg">{item.header}</div>
-					{/if}
-					{#if item.body}
-						<div class="text-sm"><BlockContent value={item.body}></BlockContent></div>
-					{/if}
-
-
+					<img src={getThumbnailImg(item.image)} class="max-w-36 self-center" />
+				{/if}
+				{#if item.header}
+					<div class="text-lg leading-snug font-bold group-hover:underline">{item.header}</div>
+				{/if}
+			</a>
+		{:else}
+			{#if item.image}
+				<img src={getThumbnailImg(item.image)} class="max-w-36 self-center" />
+			{/if}
+			{#if item.header}
+				<div class="text-lg leading-snug font-bold">{item.header}</div>
+			{/if}
+		{/if}
+		{#if item.body}
+			<div class="text-sm"><BlockContent value={item.body}></BlockContent></div>
+		{/if}
+	</div>
 </div>
-
--- src/lib/components/GridModule.svelte
+++ src/lib/components/GridModule.svelte
@@ -6,14 +6,7 @@
 
 	<div class="flex max-w-sm sm:max-w-4xl flex-wrap justify-between gap-x-4 gap-y-8 px-8 sm:px-4">
 		{#each gridItems as item}
-			{#if item.link}
-				<a href={item.link} target="_blank" class="sm:basis-1/4">
-					<GridItem {item}></GridItem>
-				</a>
-			{:else}
-				<div class="sm:basis-1/4">
-					<GridItem {item}></GridItem>
-				</div>
-			{/if}
+			<GridItem {item}></GridItem>
+
 		{/each}
 	</div>
