fe4f2bcb57dfc978dfdaa81dc71f7df93498eb8a
tdingsun <tiger.dingsun@gmail.com>
2025-05-23 13:09:02 -0400

    QA WIP

--- src/lib/components/BlockContent/BlockContentImg.svelte
+++ src/lib/components/BlockContent/BlockContentImg.svelte
@@ -4,7 +4,7 @@
     let { portableText } = $props();
 </script>
 
-<div class="my-4">
+<div class="my-6">
     <img src={getImgUrl(portableText.value)} alt={portableText.value.alt ? portableText.value.alt : ''}/>
     {#if portableText.value.caption}
         <div class="text-xs mt-1">
